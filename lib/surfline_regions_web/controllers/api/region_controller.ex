bc234f4529cd0f1d03154d53de6c6e4c11724f46
Max Helmetag <mhelmetag@gmail.com>
2018-02-01 20:27:02 -0800

    add basic json api for regions

---
+++ lib/surfline_regions_web/controllers/api/region_controller.ex
@@ -0,0 +1,19 @@
+defmodule SurflineRegionsWeb.Api.RegionController do
+  use SurflineRegionsWeb, :controller
+
+  alias SurflineRegions.Areas
+  alias SurflineRegions.Regions
+
+  action_fallback SurflineRegionsWeb.FallbackController
+
+  def index(conn, %{"area_id" => area_id}) do
+    area = Areas.get_area!(area_id)
+    regions = Regions.list_regions_by_area(area)
+    render(conn, "index.json", regions: regions)
+  end
+
+  def show(conn, %{"area_id" => _area_id, "id" => id}) do
+    region = Regions.get_region!(id)
+    render(conn, "show.json", region: region)
+  end
+end
---
+++ lib/surfline_regions_web/views/api/region_view.ex
@@ -0,0 +1,16 @@
+defmodule SurflineRegionsWeb.Api.RegionView do
+  use SurflineRegionsWeb, :view
+  alias SurflineRegionsWeb.Api.RegionView
+
+  def render("index.json", %{regions: regions}) do
+    %{data: render_many(regions, RegionView, "region.json")}
+  end
+
+  def render("show.json", %{region: region}) do
+    %{data: render_one(region, RegionView, "region.json")}
+  end
+
+  def render("region.json", %{region: region}) do
+    %{id: region.id, surfline_id: region.surfline_id, name: region.name}
+  end
+end
