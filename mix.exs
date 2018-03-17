0aee65ccd9efa814c03a971ec33a9faf27823bf3
Max Helmetag <mhelmetag@gmail.com>
2018-03-17 10:55:45 -0700

    bump version to 0.0.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SurflineRegions.Mixfile do
   def project do
     [
       app: :surfline_regions,
-      version: "0.0.3",
+      version: "0.0.4",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
